680d1053ce577f71ecc50957734fb0253c374656
RafaelFreimuth <rafaelfreimuth@outlook.com>
2021-08-06 20:39:14 -0300

    criando tabela de resultados da Api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import api from './api/api';
+import { Table} from './styles/styles'
 import './App.css';
 
 function App() {
@@ -17,7 +18,27 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Sobrenome</th>
+            <th>Email</th>
+            <th>NIS</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {funcionarios.map(funcionario => (
+            <tr key={funcionario.id}>
+              <td>{funcionario.nome}</td>
+              <td>{funcionario.sobrenome}</td>
+              <td>{funcionario.email}</td>
+              <td>{funcionario.nis}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
---
+++ src/styles/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+    margin-top: 15px;
+    width: 100%;
+
+    th {
+        background-color: rgb(230,230,255);
+        color: black;
+        padding: 10px;
+    }
+
+    td {
+        background-color: #f8f8f8;
+        color: #3e3e3e;
+        padding: 8px;
+    }
+`
